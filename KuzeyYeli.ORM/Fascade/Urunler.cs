0c60fcfc3b594ee4e985854570c5300d69b40bc7
MinikaGo <usinanoglu@gmail.com>
2016-04-13 21:47:13 +0300

    Master_test

---
+++ KuzeyYeli.ORM/Fascade/Urunler.cs
@@ -0,0 +1,38 @@
+﻿using KuzeyYeli.ORM.Entity;
+using System.Data;
+using System.Data.SqlClient;
+
+
+
+namespace KuzeyYeli.ORM.Fascade
+{
+    public class Urunler
+    {
+        public static DataTable Listele()
+        {
+            SqlDataAdapter adp = new SqlDataAdapter("UrunListele", Tools.Baglanti);
+            adp.SelectCommand.CommandType = CommandType.StoredProcedure;
+            DataTable dt = new DataTable();
+            adp.Fill(dt);
+            return dt;
+        }
+
+        public static bool Ekle(Urun entity)
+        {
+            SqlCommand komut = new SqlCommand("UrunEkle", Tools.Baglanti);
+            komut.CommandType = CommandType.StoredProcedure;
+            komut.Parameters.AddWithValue("@a", entity.UrunAdi);
+            komut.Parameters.AddWithValue("@f", entity.Fiyat);
+            komut.Parameters.AddWithValue("@s", entity.Stok);
+
+            return Tools.ExecuteNonQuery(komut);
+        }
+
+        public static bool Sil (Urun entity)
+        {
+            SqlCommand komut = new SqlCommand("UrunSil", Tools.Baglanti);        komut.CommandType = CommandType.StoredProcedure;
+            komut.Parameters.AddWithValue("@id", entity.UrunId);
+            return Tools.ExecuteNonQuery(komut);
+        }
+    }
+}
---
+++ KuzeyYeli.WinForm/Form1.cs
@@ -0,0 +1,43 @@
+﻿using KuzeyYeli.ORM.Entity;
+using KuzeyYeli.ORM.Fascade;
+using System;
+using System.Windows.Forms;
+
+namespace KuzeyYeli.WinForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Urun entity = new Urun();
+            entity.UrunAdi = textBox1.Text;
+            entity.Fiyat = numericUpDown1.Value;
+            entity.Stok = (short)numericUpDown2.Value;
+            if (!Urunler.Ekle(entity))
+                MessageBox.Show("Ürün Eklenemedi");
+
+            dataGridView1.DataSource = Urunler.Listele();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = Urunler.Listele();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count == 0) return;
+            Urun silinecek = new Urun();
+            silinecek.UrunId = (int)dataGridView1.CurrentRow.Cells["UrunID"].Value;
+            if(!Urunler.Sil(silinecek))
+                MessageBox.Show("Silinemedi");
+            dataGridView1.DataSource = Urunler.Listele();
+        }
+    }
+}
